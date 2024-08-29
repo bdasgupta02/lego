ad5506983933200ea50258a68c2dd2d25110ff4e
Bikramjit Dasgupta <bdasgupta02@gmail.com>
2024-08-29 21:49:58 +0100

    improve example

--- example.cpp
+++ example.cpp
@@ -3,7 +3,9 @@
 #include <iostream>
 
 // This example is a simple ping-pong component, where one component
-// pings the other, and we can get the total number of pings made at the end
+// pings the other, and we can get the total number of pings made at the end.
+// The fun part is that you can simply choose whichever pong component you want.
+
 
 // These tags are used to wire and dispatch functions
 // between components
@@ -15,6 +17,7 @@ struct Pong
 struct GetPings
 {};
 } // namespace tag
+
 
 // These are the actual components of the app
 template<typename NodeBase>
@@ -29,8 +32,9 @@ struct PingComponent : NodeBase
     PingCountType pings = 0;
 };
 
+// The first pong component, which just pongs when its pinged
 template<typename NodeBase>
-struct PongComponent : NodeBase
+struct PongOnce : NodeBase
 {
     using NodeBase::NodeBase;
     using PingCountType = NodeBase::Traits::PingCountType;
@@ -42,32 +46,71 @@ struct PongComponent : NodeBase
     }
 };
 
-// Type traits can be injected into the app,
+
+// The second pong component, which sends back the specified number of pongs
+template<typename NodeBase>
+struct PongNTimes : NodeBase
+{
+    using NodeBase::NodeBase;
+    using PingCountType = NodeBase::Traits::PingCountType;
+
+    void handle(tag::Ping, PingCountType numPongs)
+    {
+        auto* handler = this->getHandler();
+        for (PingCountType i = PingCountType{}; i < numPongs; ++i)
+            handler->invoke(tag::Pong{});
+    }
+};
+
+
+// Type traits can be injected into the graph,
 // if not, pass an empty struct
 struct Traits
 {
     using PingCountType = int;
 };
 
+
 // This is how the app is assembled using the Router
+// There are two variations that use shared dependencies
+
 // clang-format off
-using App = lego::Router<
+using PingPongOnce = lego::Router<
     Traits,
     lego::NodeList<
         PingComponent,
-        PongComponent
+        PongOnce
+    >
+>;
+
+using PingPongNTimes = lego::Router<
+    Traits,
+    lego::NodeList<
+        PingComponent,
+        PongNTimes
     >
 >;
 // clang-format on
 
 int main()
 {
-    App app;
+    {
+        PingPongOnce app;
 
-    auto* handler = app.getHandler();
-    for (auto i = 0u; i < 5; ++i)
+        auto* handler = app.getHandler();
         handler->invoke(tag::Ping{});
 
-    auto pings = handler->retrieve(tag::GetPings{});
-    std::cout << pings << std::endl;
+        auto pings = handler->retrieve(tag::GetPings{});
+        std::cout << pings << std::endl; // prints 1
+    }
+
+    {
+        PingPongNTimes app;
+
+        auto* handler = app.getHandler();
+        handler->invoke(tag::Ping{}, 50);
+
+        auto pings = handler->retrieve(tag::GetPings{});
+        std::cout << pings << std::endl; // prints 50
+    }
 }
